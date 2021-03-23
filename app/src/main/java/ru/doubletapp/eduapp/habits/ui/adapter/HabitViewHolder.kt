29e1c378cc9aea5a2ac85ddfabc8c2d133accc94
AntonChernyak <anton767@yandex.ru>
2021-03-24 00:00:41 +0300

    Created HabitViewHolder

--- app/src/main/java/ru/doubletapp/eduapp/habits/ui/adapter/HabitViewHolder.kt
+++ app/src/main/java/ru/doubletapp/eduapp/habits/ui/adapter/HabitViewHolder.kt
@@ -2,8 +2,25 @@ package ru.doubletapp.eduapp.habits.ui.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import ru.doubletapp.eduapp.habits.R
+import ru.doubletapp.eduapp.habits.data.model.Habit
+import ru.doubletapp.eduapp.habits.databinding.ItemHabitBinding
 
-class HabitViewHolder(view: View):RecyclerView.ViewHolder(view) {
+class HabitViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
 
+    private val binding = ItemHabitBinding.bind(itemView)
 
+    fun bind(habit: Habit, click: (View) -> Unit) {
+        binding.habitTitleTextView.text = habit.title
+        binding.habitDescriptionTextView.text = habit.description
+        binding.habitColorLabel.setBackgroundColor(habit.color)
+        binding.periodTextView.text = createPeriodString(habit)
+        binding.priorityTextView.text = habit.priority.toString()
+
+        binding.checkImageView.setOnClickListener(click)
+    }
+
+    private fun createPeriodString(habit: Habit): String{
+        return itemView.resources.getString(R.string.period_string, habit.periodCount, habit.periodDays)
+    }
 }
