9e9aeb54ee50b6e4580172e20568e596cc084ae6
AntonChernyak <anton767@yandex.ru>
2021-03-24 00:40:11 +0300

    Created HabitAdapter

---
+++ app/src/main/java/ru/doubletapp/eduapp/habits/ui/adapter/HabitAdapter.kt
@@ -0,0 +1,31 @@
+package ru.doubletapp.eduapp.habits.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.doubletapp.eduapp.habits.R
+import ru.doubletapp.eduapp.habits.data.model.Habit
+
+class HabitAdapter(
+    private val checkClick: (View) -> Unit
+): RecyclerView.Adapter<HabitViewHolder>() {
+
+    var habitsList: List<Habit> = emptyList()
+        set(value){
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HabitViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_habit, parent, false)
+        return HabitViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: HabitViewHolder, position: Int) {
+        val habit = habitsList[position]
+        holder.bind(habit, checkClick)
+    }
+
+    override fun getItemCount(): Int = habitsList.size
+}
